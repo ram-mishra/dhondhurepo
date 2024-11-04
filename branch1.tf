a5d488e567fa6bdb57a7a5262f6eb77ad0d861e1
ram-mishra <152048841+ram-mishra@users.noreply.github.com>
2024-11-05 00:27:49 +0530

    Update branch1.tf
    bur rg add

--- branch1.tf
+++ branch1.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "lul-rg" {
   name     = "lulrg1"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "bur-rg" {
+  name     = "burrg2"
+  location = "centralindia"
+}
