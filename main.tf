9543232d3e73444bd9d203903b333c890efdc888
ram-mishra <152048841+ram-mishra@users.noreply.github.com>
2024-11-08 20:30:35 +0530

    Update main.tf
    neddadd in main

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "akshay-rgs" {
   name     = "akshayrg"
   location = "centralindia"
   }
+resource "azurerm_resource_group" "newaddinmain " {
+  name     = "newaad-main"
+  location = "eastus"
+  }
