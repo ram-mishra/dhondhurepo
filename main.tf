0b6260b2a2735fbe5c8ccc086a660333cff7611a
Ram 1. Mishra (EXT-Nokia) <ram.1.mishra.ext@nokia.com>
2024-11-12 00:05:47 +0530

    add delhi-rg

--- main.tf
+++ main.tf
@@ -6,3 +6,7 @@ resource "azurerm_resource_group" "newaddinmain " {
   name     = "newaad-main"
   location = "eastus"
   }
+  resource "azurerm_resource_group" "delhi " {
+  name     = "delhi-rg"
+  location = "southindia"
+  }
